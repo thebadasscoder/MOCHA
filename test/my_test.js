4439403a678c038439b83a16e0ae294a69875b6f
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2016-12-20 12:03:53 -0500

    updating changes for test case

--- test/my_test.js
+++ test/my_test.js
@@ -1,9 +1,11 @@
 const expect = require('chai').expect; //Chai is an assertion library that I we will use
 
 //In this test case I am describing what my function should do.
-//I am expecting 3 to equal 2; 
+//I am expecting 3 to equal 2; (This is an expectation in Chai)
 describe('my test suite', function(){
 	it('passes successfully', function(){
 		expect(3).to.eql(3);
 	});
 });
+
+//
